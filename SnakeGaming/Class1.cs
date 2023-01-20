aa22a19b2a306ea3adc2d9b2af3ab5388adb495a
Asbjørn Juhl Holm Pedersen <asbj1702@elev.heguddannelser.dk>
2023-01-20 13:29:40 +0100

    Basic

---
+++ SnakeGaming/Class1.cs
@@ -0,0 +1,154 @@
+﻿namespace SnakeConsole
+{
+    internal class Position
+    {
+        public int row;
+        public int col;
+        public Position(int r, int c)
+        {
+            row = r;
+            col = c;
+        }
+    }
+    internal class Board
+    {
+        int rows;
+        int cols;
+        public Board(int r, int c)
+        {
+            rows = r;
+            cols = c;
+            Console.CursorVisible = false;
+        }
+        public void Render()
+        {
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (row == 0 || col == 0 || row == rows - 2 || col == cols - 2)
+                    {
+                        Console.SetCursorPosition(col, row);
+                        Console.Write('-');
+                    }
+                }
+            }
+        }
+    }
+    internal class Snake
+    {
+        private Queue<Position> body;
+        private Dictionary<Position, bool> occupiedPositions;
+        Position clearPosition = null;
+        public int rows, cols;
+        Direction direction = Direction.None;
+        private Random random;
+        public bool collision;
+        public Snake(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            random = new Random();
+            body = new Queue<Position>();
+            occupiedPositions = new Dictionary<Position, bool>();
+            int trow, tcol;
+            trow = rows / 2 + random.Next(-3, 3);
+            tcol = cols / 2 + random.Next(-7, 7);
+            Position newHead = new Position(trow, tcol);
+            body.Enqueue(newHead);
+            occupiedPositions.Add(newHead, true);
+            direction = Direction.None; // Don't move until .. .
+            collision = false;
+        }
+        public void Update()
+        {
+            Position head = body.Last();
+            Position tail = null;
+            Position newHead;
+            // Debugging:
+            bool testNu = true;
+            if (testNu)
+            {
+                Console.SetCursorPosition(cols / 2, rows / 2);
+                Console.Write(" ");
+                Console.SetCursorPosition(cols / 2, rows / 2);
+                Console.Write("r = " + head.row + ", c = " + head.col);
+                Console.SetCursorPosition(cols / 2, rows / 2 + 1);
+                Console.Write(" ");
+                Console.SetCursorPosition(cols / 2, rows / 2 + 1);
+                Console.Write("collsion = " + collision);
+            }
+            // Hvilken retning?
+            switch (direction)
+            {
+                case Direction.Up:
+                    // If at top row, collision with wall:
+                    if (head.row < 2)
+                    {
+                        collision = true;
+                        return;
+                    }
+                    // Heads new position is added to body:
+                    newHead = new Position(
+                    head.row - 1,
+                    head.col
+                    );
+                    body.Enqueue(newHead);
+                    occupiedPositions.Add(newHead, true);
+                    // Remove tail:
+                    tail = body.Dequeue();
+                    clearPosition = tail;
+                    occupiedPositions.Remove(tail);
+                    break;
+                case Direction.Down:
+                    break;
+                case Direction.Left:
+                    break;
+                case Direction.Right:
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void Render()
+        {
+            foreach (var item in body)
+            {
+                Console.SetCursorPosition(item.col, item.row);
+                Console.Write('*');
+            }
+            // Overwrite old position:
+            if (clearPosition != null)
+            {
+                Console.SetCursorPosition(clearPosition.col, clearPosition.row);
+                Console.Write(' ');
+            }
+        }
+        public void ReadInput()
+        {
+            if (!Console.KeyAvailable)
+                return;
+            ConsoleKeyInfo key = Console.ReadKey();
+            switch (key.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    direction = Direction.Left;
+                    break;
+                case ConsoleKey.RightArrow:
+                    direction = Direction.Right;
+                    break;
+                case ConsoleKey.UpArrow:
+                    direction = Direction.Up;
+                    break;
+                case ConsoleKey.DownArrow:
+                    direction = Direction.Down;
+                    break;
+            }
+        }
+        public bool IsCollision(Position position)
+        {
+            // Undersøger om position allerede er optaget:
+            return occupiedPositions.ContainsKey(position);
+        }
+    }
+}
--- SnakeGaming/Program.cs
+++ SnakeGaming/Program.cs
@@ -1,11 +1,46 @@
-﻿namespace SnakeGaming
+﻿using System.Xml.Linq;
+
+namespace SnakeConsole
 {
+    public enum Direction : int
+    {
+        Up, Down, Left, Right, None
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine();
+            int rows = Console.WindowHeight;
+            int cols = Console.WindowWidth;
+            // typ: 30 x 120 (rows x columns)
+            Board board = new Board(rows, cols);
+            Snake snake = new Snake(rows, cols);
+            // Render Board og Snake:
+            board.Render();
+            snake.Render();
+            const float TARGET_FPS = 4f; // Frame rate (ønsket)
+            const float TARGET_FRAME_TIME = 1f / TARGET_FPS; // Tid per frame
+            DateTime previousFrameTime;
+            float elapsedTime = 0f;
+            bool gameIsRunning = true;
+            while (gameIsRunning)
+            {
+                // Gem nuværende tidspunkt:
+                previousFrameTime = DateTime.UtcNow;
+                snake.Update();
+                snake.Render();
+                snake.ReadInput();
+                // Beregn faktisk løkketid (cirka):
+                elapsedTime = (float)(DateTime.UtcNow - previousFrameTime).TotalSeconds;
+                if (elapsedTime < TARGET_FRAME_TIME)
+                {
+                    // Hvis en frame tid ikke er forløbet endnu, sov lidt:
+                    int sleepTime = (int)((TARGET_FRAME_TIME - elapsedTime) * 1000);
+                    System.Threading.Thread.Sleep(sleepTime);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
+
